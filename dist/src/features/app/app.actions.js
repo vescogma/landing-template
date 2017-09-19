87fc76f4bf3ecd33fb5869c0f739bcfa8e368545
Miguel Vesco <vescogma@gmail.com>
2017-09-19 01:45:11 -0400

    fixed dist

--- dist/src/features/app/app.actions.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.sendEmail = sendEmail;
-async function sendEmail() {
-  fetch('/email', { method: 'get' });
-}
--- dist/src/features/app/app.constants.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-const SEND_EMAIL = exports.SEND_EMAIL = 'app/services/send_email';
